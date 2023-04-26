e148d3b010c6aabd2018c5f94650e3d6a57417ab
Josué Castellani <josue.castellani@student.hepl.be>
2023-04-26 17:36:00 +0200

    260417H35

---
+++ src/InterfaceConnection.java
@@ -0,0 +1,71 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class InterfaceConnection  extends JDialog
+{
+
+
+
+        private String login;
+        private String motDePasse;
+        private boolean Confirmer;
+    private JPanel Main;
+    private JLabel LoginLabel;
+    private JLabel InfoLabel;
+    private JTextField Logintextfield;
+    private JTextField mdpfield;
+    private JButton ButtonConfirmer;
+    private JLabel mdpLabel;
+
+    public InterfaceConnection(JFrame parent,boolean modal,String titre)
+        {
+            super(parent,modal);
+            setTitle(titre);
+            setContentPane(Main);
+            setLocationRelativeTo(null);
+            pack();
+            Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+            setLocation((screen.width - this.getSize().width)/2,(screen.height - this.getSize().height)/2);
+
+            Confirmer = false;
+            ButtonConfirmer.addActionListener(new ActionListener()
+            {
+                @Override
+                public void actionPerformed(ActionEvent e)
+                {
+                    login = Logintextfield.getText();
+                    motDePasse = mdpfield.getText();
+                    Confirmer = true;
+                    setVisible(false);
+                }
+            });
+
+        }
+        public static void main(String[] args)
+        {
+            InterfaceConnection interfaceConnection = new InterfaceConnection(null,true,"Entrée en session...");
+            interfaceConnection.setVisible(true);
+            if (interfaceConnection.isConfirmer())
+            {
+                System.out.println("Login = " + interfaceConnection.getLogin());
+                System.out.println("Mot de passe = " + interfaceConnection.getMotDePasse());
+            }
+            interfaceConnection.dispose();
+        }
+
+        public String getLogin()
+        {
+            return login;
+        }
+
+        public String getMotDePasse()
+        {
+            return motDePasse;
+        }
+
+        public boolean isConfirmer()
+        {
+            return Confirmer;
+        }
+}
