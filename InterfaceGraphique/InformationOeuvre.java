612991d1158f8d521fae9187cad1cfa4fd3c034a
Josué <114358135+JosueC05@users.noreply.github.com>
2023-04-27 20:14:48 +0200

    Add files via upload

---
+++ InterfaceGraphique/InformationOeuvre.java
@@ -0,0 +1,13 @@
+package InterfaceGraphique;
+
+import javax.swing.*;
+
+public class InformationOeuvre
+{
+
+    private JPanel panel1;
+    private JList list1;
+    private JRadioButton favorisRadioButton;
+    private JSlider slider1;
+
+}
