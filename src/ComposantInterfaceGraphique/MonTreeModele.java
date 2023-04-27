b2f7dbe3384ac5a20c6d6f6accd539fbb0a8066c
Josué Castellani <josue.castellani@student.hepl.be>
2023-04-27 22:33:48 +0200

    270422H33

---
+++ src/ComposantInterfaceGraphique/MonTreeModele.java
@@ -0,0 +1,12 @@
+package ComposantInterfaceGraphique;
+
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeNode;
+
+public class MonTreeModele extends DefaultTreeModel
+{
+    public MonTreeModele(TreeNode root)
+    {
+        super(root);
+    }
+}
